5ac47af04b313fa504cda410f0bf2872d0d4cf88
Mason Browne <mason@massivelyfun.com>
2012-08-14 12:19:17 -0700

    Fix global leak of Hashtable.

--- src-common/commons/collection/hashSet.js
+++ src-common/commons/collection/hashSet.js
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -27,86 +27,87 @@
  * Website: http://www.timdown.co.uk/jshashtable/
  */
 
-Hashtable = require("./hashtable");
-
-function HashSet(hashingFunction, equalityFunction) {
+var HashSet = (function(){
+  var Hashtable = require("./hashtable");
+  function HashSet(hashingFunction, equalityFunction) {
     var hashTable = new Hashtable(hashingFunction, equalityFunction);
 
     this.add = function(o) {
-        hashTable.put(o, true);
+      hashTable.put(o, true);
     };
 
     this.addAll = function(arr) {
-        var i = arr.length;
-        while (i--) {
-            hashTable.put(arr[i], true);
-        }
+      var i = arr.length;
+      while (i--) {
+        hashTable.put(arr[i], true);
+      }
     };
 
     this.values = function() {
-        return hashTable.keys();
+      return hashTable.keys();
     };
 
     this.remove = function(o) {
-        return hashTable.remove(o) ? o : null;
+      return hashTable.remove(o) ? o : null;
     };
 
     this.contains = function(o) {
-        return hashTable.containsKey(o);
+      return hashTable.containsKey(o);
     };
 
     this.clear = function() {
-        hashTable.clear();
+      hashTable.clear();
     };
 
     this.size = function() {
-        return hashTable.size();
+      return hashTable.size();
     };
 
     this.isEmpty = function() {
-        return hashTable.isEmpty();
+      return hashTable.isEmpty();
     };
 
     this.clone = function() {
-        var h = new HashSet(hashingFunction, equalityFunction);
-        h.addAll(hashTable.keys());
-        return h;
+      var h = new HashSet(hashingFunction, equalityFunction);
+      h.addAll(hashTable.keys());
+      return h;
     };
 
     this.intersection = function(hashSet) {
-        var intersection = new HashSet(hashingFunction, equalityFunction);
-        var values = hashSet.values(), i = values.length, val;
-        while (i--) {
-            val = values[i];
-            if (hashTable.containsKey(val)) {
-                intersection.add(val);
-            }
+      var intersection = new HashSet(hashingFunction, equalityFunction);
+      var values = hashSet.values(), i = values.length, val;
+      while (i--) {
+        val = values[i];
+        if (hashTable.containsKey(val)) {
+          intersection.add(val);
         }
-        return intersection;
+      }
+      return intersection;
     };
 
     this.union = function(hashSet) {
-        var union = this.clone();
-        var values = hashSet.values(), i = values.length, val;
-        while (i--) {
-            val = values[i];
-            if (!hashTable.containsKey(val)) {
-                union.add(val);
-            }
+      var union = this.clone();
+      var values = hashSet.values(), i = values.length, val;
+      while (i--) {
+        val = values[i];
+        if (!hashTable.containsKey(val)) {
+          union.add(val);
         }
-        return union;
+      }
+      return union;
     };
 
     this.isSubsetOf = function(hashSet) {
-        var values = hashTable.keys(), i = values.length;
-        while (i--) {
-            if (!hashSet.contains(values[i])) {
-                return false;
-            }
+      var values = hashTable.keys(), i = values.length;
+      while (i--) {
+        if (!hashSet.contains(values[i])) {
+          return false;
         }
-        return true;
+      }
+      return true;
     };
-}
+  }
+  return HashSet
+})();
 
 module.exports = HashSet
-
